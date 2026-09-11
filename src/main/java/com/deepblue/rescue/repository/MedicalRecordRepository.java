c2a18970a5afe67a9b9a7b7891b1fea7bdd64896
Daniel <davergaraf@unimagdalena.edu.co>
2026-09-11 00:54:52 -0500

    fix(persistence): change MedicalRecordRepository to interface

--- src/main/java/com/deepblue/rescue/repository/MedicalRecordRepository.java
+++ src/main/java/com/deepblue/rescue/repository/MedicalRecordRepository.java
@@ -2,7 +2,8 @@ package com.deepblue.rescue.repository;
 
 import com.deepblue.rescue.domain.MedicalRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class MedicalRecordRepository extends JpaRepository<MedicalRecord, Long>
-{
+@Repository
+public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
 }
