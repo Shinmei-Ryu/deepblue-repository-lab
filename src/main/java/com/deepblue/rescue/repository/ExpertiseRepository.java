c373c73913d5ad19926b9724396ab34fc1240bff
Daniel <davergaraf@unimagdalena.edu.co>
2026-09-11 00:44:01 -0500

    feat(persistence): add Query methods to ExpertiseRepository

--- src/main/java/com/deepblue/rescue/repository/ExpertiseRepository.java
+++ src/main/java/com/deepblue/rescue/repository/ExpertiseRepository.java
@@ -2,7 +2,12 @@ package com.deepblue.rescue.repository;
 
 import com.deepblue.rescue.domain.Expertise;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class ExpertiseRepository extends JpaRepository<Expertise, Long>
-{
+import java.util.Optional;
+
+@Repository
+public interface ExpertiseRepository extends JpaRepository<Expertise, Long> {
+
+    Optional<Expertise> findByNameIgnoreCase(String name);
 }
