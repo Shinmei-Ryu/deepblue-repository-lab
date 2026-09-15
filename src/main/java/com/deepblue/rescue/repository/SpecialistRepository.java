a58d86190a4d4edee2bed6131d28dbcb6368adbf
Daniel <davergaraf@unimagdalena.edu.co>
2026-09-14 21:04:29 -0500

    feat(service):add new method needed on SpecialistRepository

--- src/main/java/com/deepblue/rescue/repository/SpecialistRepository.java
+++ src/main/java/com/deepblue/rescue/repository/SpecialistRepository.java
@@ -7,9 +7,14 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SpecialistRepository extends JpaRepository<Specialist, Long> {
+
+    Optional<Specialist> findByProfessionalCode(
+            String professionalCode);
+
 
     @Query("""
             select distinct s
